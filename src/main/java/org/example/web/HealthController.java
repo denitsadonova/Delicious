01da138e595780eebfbe4a99e6c95ec4fe7ae711
Denitsa Donova <denitsadonova@gmail.com>
2025-03-15 13:51:11 +0200

    Today I added a new Entity - MenuEntity and made some functionality for it.

--- src/main/java/org/example/web/HealthController.java
+++ src/main/java/org/example/web/HealthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HealthController {
         @GetMapping("/health")
-        public String showHealthPage(Model model) {
+        public String healthPage(Model model) {
             model.addAttribute("vitamins", Vitamins.values());
             model.addAttribute("minerals", Minerals.values());
             return "health";
--- src/main/java/org/example/web/RecipeController.java
+++ src/main/java/org/example/web/RecipeController.java
@@ -3,16 +3,15 @@ package org.example.web;
 import jakarta.validation.Valid;
 import org.example.models.binding.RecipeAddBindingModel;
 import org.example.models.entity.IngredientEntity;
+import org.example.models.entity.MenuEntity;
 import org.example.repository.IngredientRepository;
+import org.example.repository.MenuRepository;
 import org.example.service.IngredientService;
 import org.example.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -22,10 +21,12 @@ import java.util.List;
 public class RecipeController {
     private final RecipeService recipeService;
     private final IngredientService ingredientService;
+    private final MenuRepository menuRepository;
 
-    public RecipeController(RecipeService recipeService, IngredientService ingredientService) {
+    public RecipeController(RecipeService recipeService, IngredientService ingredientService, MenuRepository menuRepository) {
         this.recipeService = recipeService;
         this.ingredientService = ingredientService;
+        this.menuRepository = menuRepository;
     }
 
     @GetMapping("/add/recipe")
@@ -49,6 +50,7 @@ public class RecipeController {
 public String successAddRecipe() {
         return "success-add-recipe";
     }
+
     @GetMapping("/all/recipes")
     public String allRecipes(Model model){
         model.addAttribute("recipes", recipeService.getAllRecipes());
